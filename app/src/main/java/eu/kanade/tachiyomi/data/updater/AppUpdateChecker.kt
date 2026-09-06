80754cf678c888081cdef499450691dd25cd9497
mkonic <mkonic676@gmail.com>
2026-09-06 15:20:22 +0300

    app: check for updates from this fork

--- app/src/main/java/eu/kanade/tachiyomi/data/updater/AppUpdateChecker.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/updater/AppUpdateChecker.kt
@@ -95,12 +95,8 @@ class AppUpdateChecker(
 
 val GITHUB_REPO: String by lazy { getGithubRepo() }
 
-fun getGithubRepo(peekIntoPreview: Boolean = false): String =
-    if (isPreviewBuildType || peekIntoPreview) {
-        "komikku-app/komikku-preview"
-    } else {
-        "komikku-app/komikku"
-    }
+@Suppress("UNUSED_PARAMETER")
+fun getGithubRepo(peekIntoPreview: Boolean = false): String = "mKonic/komikku"
 
 val RELEASE_TAG: String by lazy { getReleaseTag() }
 
