517ce4435d1974b360937dd87f98cf92818516a2
mkonic <mkonic676@gmail.com>
2026-09-06 15:55:41 +0300

    app: stop double-prefixing the release tag

--- app/src/main/java/eu/kanade/tachiyomi/data/updater/AppUpdateChecker.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/updater/AppUpdateChecker.kt
@@ -104,7 +104,7 @@ fun getReleaseTag(peekIntoPreview: Boolean = false): String =
     if (isPreviewBuildType || peekIntoPreview) {
         "r${BuildConfig.COMMIT_COUNT}"
     } else {
-        "v${BuildConfig.VERSION_NAME}"
+        BuildConfig.VERSION_NAME
     }
 
 val RELEASE_URL = "https://github.com/$GITHUB_REPO/releases/tag/$RELEASE_TAG"
