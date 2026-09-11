7978cc9761de9cced9643d637a0736f8587997ec
mkonic <mkonic676@gmail.com>
2026-09-11 19:13:00 +0300

    reader: release a decoded page's texture when it can no longer be used

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/viewer/webgpu/WebGpuViewer.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/viewer/webgpu/WebGpuViewer.kt
@@ -1156,6 +1156,7 @@ open class WebGpuViewer(
                     pager.state.invalidate()
                 } else {
                     if (pageInCache(page)) page.state = PageState.IDLE
+                    imagePage.cleanup()
                 }
             }
         }
