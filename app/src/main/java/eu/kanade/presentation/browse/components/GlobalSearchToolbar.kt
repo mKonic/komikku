6019d53b15332286e2d08a39d98d7161f5aef2da
mkonic <mkonic676@gmail.com>
2026-09-11 19:13:00 +0300

    browse: size the global search divider to the chip row

--- app/src/main/java/eu/kanade/presentation/browse/components/GlobalSearchToolbar.kt
+++ app/src/main/java/eu/kanade/presentation/browse/components/GlobalSearchToolbar.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -88,6 +89,7 @@ fun GlobalSearchToolbar(
                 .horizontalScroll(rememberScrollState())
                 .padding(horizontal = MaterialTheme.padding.small),
             horizontalArrangement = Arrangement.spacedBy(MaterialTheme.padding.small),
+            verticalAlignment = Alignment.CenterVertically,
         ) {
             // TODO: make this UX better; it only applies when triggering a new search
             if (!hideSourceFilter) {
@@ -126,7 +128,7 @@ fun GlobalSearchToolbar(
                     },
                 )
 
-                VerticalDivider()
+                VerticalDivider(modifier = Modifier.height(FilterChipDefaults.Height))
             }
 
             FilterChip(
